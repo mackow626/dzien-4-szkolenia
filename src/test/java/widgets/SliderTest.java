3c05694fba26033d0ff944672024ecabc6988076
SG0953457 <@WSX3edc$RFV>
2020-11-13 14:25:10 +0100

    slider advance

--- src/test/java/widgets/SliderTest.java
+++ src/test/java/widgets/SliderTest.java
@@ -3,31 +3,50 @@ package widgets;
 import basic.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SliderTest extends TestBase {
 
     // Zadanie:
-    // Przesun slider do wartości 50 -> 30 -> 80
+    // Przesun slider do wartości 50 -> 30 -> 30 -> 80
 
     @Test
     public void shouldMoveSliderToExpectedValue() {
         getDriver().get("http://seleniumui.moderntester.pl/slider.php");
 
-        Integer expectedValue = 50;
-        Integer expectedValue2 = 30;
-        Integer expectedValue3 = 80;
+        moveTo(50);
+        Assert.assertEquals(getSliderValue(),50);
+        moveTo(30);
+        Assert.assertEquals(getSliderValue(),30);
+        moveTo(30);
+        Assert.assertEquals(getSliderValue(),30);
+        moveTo(80);
+        Assert.assertEquals(getSliderValue(),80);
+    }
 
-        while (!getDriver().findElement(By.id("custom-handle")).getText().equals(expectedValue.toString())) {
-            getDriver().findElement(By.id("custom-handle")).sendKeys(Keys.ARROW_RIGHT);
-        }
+    // metoda spradzająca w którą stronę ma się przesunąć slider, a następnie zaczynająca go przesuwać
+    private void moveTo(int expectedSliderValue) {
+        int actualSliderValue = getSliderValue();
 
-        while (!getDriver().findElement(By.id("custom-handle")).getText().equals(expectedValue2.toString())) {
-            getDriver().findElement(By.id("custom-handle")).sendKeys(Keys.ARROW_LEFT);
+        if (actualSliderValue > expectedSliderValue) {
+            moveSlider(expectedSliderValue, Keys.ARROW_LEFT);
+        } else if (actualSliderValue < expectedSliderValue) {
+            moveSlider(expectedSliderValue, Keys.ARROW_RIGHT);
         }
+    }
 
-        while (!getDriver().findElement(By.id("custom-handle")).getText().equals(expectedValue3.toString())) {
-            getDriver().findElement(By.id("custom-handle")).sendKeys(Keys.ARROW_RIGHT);
+    // wysyłanie do slidera strzałkę w lewo / prawo aby go przesunąć
+    // tak długo aż wartość na sliderze będzie równa wartości oczekiwanej
+    private void moveSlider(int expectedSliderValue, Keys key) {
+        while (getSliderValue() != expectedSliderValue) {
+            getDriver().findElement(By.id("custom-handle")).sendKeys(key);
         }
+    }
+
+    // pobieranie textu wyświetlanego w sliderze (jako tekst) i parsowanie go do inta (liczby)
+    private int getSliderValue() {
+        String sliderValueAsText = getDriver().findElement(By.id("custom-handle")).getText();
+        return Integer.parseInt(sliderValueAsText); // "50" -> 50
     }
 }
